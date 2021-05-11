db0830f97f2c9a77817b36bed2e491314c9e59ff
Tejash Datta <tejashdatt@gmail.com>
2021-05-11 16:30:59 +0530

    enable webpacker dev server

--- docker-setup-commands.sh
+++ docker-setup-commands.sh
@@ -1,3 +1,4 @@
+# setup
 docker-compose run --no-deps app rails new . --force --database=postgresql
 sudo chown -R $USER:$USER .
 docker-compose build
@@ -7,3 +8,7 @@ docker-compose run app rake db:create
 # rebuild
 docker-compose run app bundle install
 docker-compose up --build
+
+# common commands
+docker-compose up
+docker-compose run app rails
