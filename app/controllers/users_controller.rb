d5dca1702c5db200d72806fae375d61f2273aa5c
Tejash Datta <tejashdatt@gmail.com>
2021-05-18 01:43:46 +0530

    japanize messages, css change in form item

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: "User was successfully created."
+      redirect_to @user, notice: "ユーザーの作成が合格しました."
     else
       render :new
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user = find_user(params[:id])
     
     if @user.update(user_params)
-      redirect_to @user, notice: "User was successfully updated."
+      redirect_to @user, notice: "ユーザーの更新が合格しました."
     else
       render :edit
     end
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
 
   def destroy
     find_user(params[:id]).destroy
-    redirect_to users_url, notice: "User was successfully destroyed."
+    redirect_to users_url, notice: "ユーザーの削除が合格しました."
   end
 
   private
