db353eabfbeff132f1b850203ad8b5560380a792
Tejash Datta <tejashdatt@gmail.com>
2021-05-21 21:21:57 +0530

    login logout done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/log", to: "static_pages#log", as: :log
   get "/sql", to: "static_pages#sql", as: :sql
+
+  get "/login", to: "authentication#new", as: :login
+  post "/login", to: "authentication#create"
+  delete "/logout", to: "authentication#destroy", as: :logout
+
   resources :users
 end
