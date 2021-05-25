2cb1a6ffa465312f1ef4c7ec4eb91023f82c8010
Tejash Datta <tejashdatt@gmail.com>
2021-05-25 22:33:51 +0530

    health information link in users page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require "factory_bot_rails"
 
-assign_all_permissions(
-  FactoryBot.create(:user, name: "a endo", email: "a_endo@ga-tech.co.jp", password: "a_endo@ga-tech.co.jp")
-)
+a_endo = FactoryBot.create(:user, name: "a endo", email: "a_endo@ga-tech.co.jp", password: "a_endo@ga-tech.co.jp")
+assign_all_permissions(a_endo)
+FactoryBot.create(:health_information, user: a_endo)
+
 FactoryBot.create_list(:user, 10)
