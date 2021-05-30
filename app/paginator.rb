5417f155520584be3713ce92c5e9e52ce5e223e6
Tejash Datta <tejashdatt@gmail.com>
2021-05-31 02:30:24 +0530

    user only constant for per page

--- app/paginator.rb
+++ app/paginator.rb
@@ -6,14 +6,13 @@ class Paginator
   def initialize(model, page_param)
     @model = model
     @page = page_param.nil? ? 1 : page_param.to_i
-    @per_page = PER_PAGE
   end
 
   def last_page
-    @model.count.zero? ? 1 : (@model.count - 1) / @per_page + 1
+    @model.count.zero? ? 1 : (@model.count - 1) / PER_PAGE + 1
   end
 
   def items
-    @model.all.limit(@per_page).offset((@page - 1) * @per_page)
+    @model.all.limit(PER_PAGE).offset((@page - 1) * PER_PAGE)
   end
 end
