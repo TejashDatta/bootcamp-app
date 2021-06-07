f1877194ce4f13c4706748357656d633d3108e82
Tejash Datta <tejashdatt@gmail.com>
2021-06-08 04:45:56 +0530

    test for search param validation

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -36,4 +36,12 @@ RSpec.describe Employee, type: :model do
       expect(Employee.searchable_columns).to eq(%w[name date_of_joining department])
     end
   end
+
+  describe "::validate_search_params" do
+    it "validates that date_of_joining is in proper format" do
+      expect(
+        Employee.validate_search_params({ date_of_joining: "12 Jan 2021" })[:date_of_joining].length
+      ).to eq 1
+    end
+  end
 end
