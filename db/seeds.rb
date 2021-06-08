c33df74a0837739e559f731ca5cae77193d06fe1
Tejash Datta <tejashdatt@gmail.com>
2021-06-09 03:48:31 +0530

    update seeds for traveler and investor

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,14 @@ assign_permissions(
   FactoryBot.create(:user, name: "a endo", email: "a_endo@ga-tech.co.jp", password: "a_endo@ga-tech.co.jp")
 )
 FactoryBot.create_list(:user, 75)
-FactoryBot.create_list(:traveler, 75)
+FactoryBot.create_list(:traveler, 25).each do |traveler|
+  FactoryBot.create(:passport, traveler: traveler)
+end
 
-FactoryBot.create(:investor)
+FactoryBot.create_list(:investor, 10).each do |investor|
+  FactoryBot.create(:stock_investment, investor: investor)
+end
+
 FactoryBot.create(:technology, name: "ruby")
 FactoryBot.create(:technology, name: "rails")
 FactoryBot.create(:technology, name: "python")
