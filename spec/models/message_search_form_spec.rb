3f2667c45ee8bde83d6ede00e2ef060d34370354
Tejash Datta <tejashdatt@gmail.com>
2021-06-23 00:40:08 +0530

    message search form spec refactoring

--- spec/models/message_search_form_spec.rb
+++ spec/models/message_search_form_spec.rb
@@ -4,26 +4,40 @@ RSpec.describe MessageSearchForm do
   let(:user) { create(:user) }
 
   describe "#search" do
-    it "searches for messages with sender email" do
-      sender = create(:user)
-      create(:message, sender: sender, receiver: user)
-      expect(MessageSearchForm.new(user: user, params: { sender_email: sender.email }).search.size).to eq 1
+    shared_examples "returns a search result" do
+      it "returns a search result" do
+        expect(MessageSearchForm.new(user: user, params: search_params).search.size).to eq 1
+      end
     end
 
-    it "searches for messages with receiver email" do
-      receiver = create(:user)
-      create(:message, sender: user, receiver: receiver)
-      expect(MessageSearchForm.new(user: user, params: { receiver_email: receiver.email }).search.size).to eq 1
+    context "searching with sender_email" do
+      let(:sender) { create(:user) }
+      let(:search_params) { { sender_email: sender.email } }
+      before { create(:message, sender: sender, receiver: user) }
+
+      include_examples "returns a search result"
     end
 
-    it "searches for messages with subject" do
-      create(:message, sender: user, subject: "example subject")
-      expect(MessageSearchForm.new(user: user, params: { subject: "example" }).search.size).to eq 1
+    context "searching with receiver_email" do
+      let(:receiver) { create(:user) }
+      let(:search_params) { { receiver_email: receiver.email } }
+      before { create(:message, sender: user, receiver: receiver) }
+
+      include_examples "returns a search result"
     end
 
-    it "searches for messages with content" do
-      create(:message, sender: user, content: "example content")
-      expect(MessageSearchForm.new(user: user, params: { content: "example" }).search.size).to eq 1
+    context "searching with subject" do
+      let(:search_params) { { subject: "example" } }
+      before { create(:message, sender: user, subject: "example subject") }
+
+      include_examples "returns a search result"
+    end
+
+    context "searching with content" do
+      let(:search_params) { { content: "example" } }
+      before { create(:message, sender: user, content: "example content") }
+
+      include_examples "returns a search result"
     end
   end
 end
